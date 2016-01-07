cfd719564f1ce7c86e8cc80e7e1c102c09aaaee4
andela-ggikera <githinji.gikera@andela.com>
2016-01-07 13:04:22 +0300

    (test) Bucketlist item retrieval test is failing

--- tests/test_bucketlist_items.py
+++ tests/test_bucketlist_items.py
@@ -4,7 +4,7 @@ import json
 
 
 class TestBucketListItem(BaseTestCase):
-    """ Tests an actionable bucketlist and all items operations in it """
+    """Tests an actionable bucketlist and all items operations in it """
 
     # ENDPOINT: POST /bucketlists/<id>/items
     def test_user_can_create_new_bucketlist_item(self):
@@ -24,5 +24,17 @@ class TestBucketListItem(BaseTestCase):
         self.assertEqual(rv.status_code, 201)
 
     def test_user_can_retrieve_bucketlist_items(self):
-         """Tests users can retrieve an item inside their bucketlist """
-         pass
+        """Tests users can retrieve a bucketlist item by id """
+        res = self.client().post('/auth/login', data=self.user)
+        res_data = json.loads(res.data)
+        jwt_token = res_data.get('token')
+        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
+        rv = self.client().post(
+            '/bucketlists',
+            data={'name': 'Eat pray and love'}, headers=headers)
+        self.assertEqual(rv.status_code, 201)
+        rv = self.client().get(
+            '/bucketlists/1/items',
+            data={"name": "Go to Osteria Francescana and dine lavishly"},
+            headers=headers)
+        self.assertEqual(rv.status_code, 201)
