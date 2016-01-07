213e3fcdb5e07a4b23dcef0fc896eb3cd12d3067
andela-ggikera <githinji.gikera@andela.com>
2016-01-08 01:13:52 +0300

    (fix) Import test suites in one executable test file

---
+++ test.py
@@ -0,0 +1,11 @@
+from tests import test_auth, test_bucketlist, test_bucketlist_items
+import unittest
+
+suite = unittest.TestLoader().loadTestsFromTestCase(
+    test_auth.AuthTestCase)
+suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+    test_bucketlist.BucketListTestCase))
+suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+    test_bucketlist_items.BucketListItemTestCase))
+
+unittest.TextTestRunner(verbosity=2).run(suite)
