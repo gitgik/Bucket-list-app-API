58de8def34d0a647c586e79fa4adf2b144e418e8
andela-ggikera <githinji.gikera@andela.com>
2016-01-07 07:46:37 +0300

    (chore) add server script

---
+++ run.py
@@ -0,0 +1,24 @@
+import sys
+from app.app import create_app
+
+environments = {
+    'test': 'instance.config.TestingConfig',
+    'development': 'instance.config.DevelopmentConfig',
+    'production': 'instance.config.ProductionConfig'
+}
+
+try:
+    if sys.argv[1] in environments.keys():
+        app = create_app(environments.get(sys.argv[1]))
+        # Run the app
+        app.run(debug=app.config.get('TESTING'))
+    else:
+        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            print ('Usage: python run.py <environment>')
+            print ('\n Available options')
+            print ('\t -- production \n\t -- development \n\t -- test')
+except IndexError:
+    # Handle missing arguments or more unfriendly options than required
+    print ("Missing options: use [python run.py -h] \
+         to get list of available options")
+exit()
