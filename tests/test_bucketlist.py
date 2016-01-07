ac1be621446d541cae58e50889775468ddb73f58
andela-ggikera <githinji.gikera@andela.com>
2016-01-07 11:52:19 +0300

    (test) add bucketlist deletion test

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -3,10 +3,11 @@ import json
 
 
 class BucketListTestCase(BaseTestCase):
-    """ Tests Users can access their bucketlist(s) """
+    """Tests various endpoints to a bucketlist """
 
     # ENDPOINT: GET '/bucketlists'
     def test_user_can_get_bucketlist_items(self):
+        """Tests users can retrieve their bucketlists """
         login_res = self.client().post('/auth/login', data=self.user)
         login_data = json.loads(login_res.data)
         jwt_token = login_data.get('token')
@@ -22,23 +23,9 @@ class BucketListTestCase(BaseTestCase):
         # logout the user after testing
         self.client().get('/aith/logout', headers=headers)
 
-    # ENDPOINT: POST '/bucketlists'
-    # Allows users to create a new bucketlist
-    def test_user_can_create_bucketlist(self):
-        res = self.client().post('/auth/login', data=self.user)
-        res_data = json.loads(res.data)
-        jwt_token = res_data.get('token')
-        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
-        rv = self.client().post(
-            '/bucketlists',
-            data={'name': 'Eat pray and love'}, headers=headers)
-        self.assertEqual(rv.status_code, 201)
-        # logout the user after testing
-        self.client().get('/auth/logout', headers=headers)
-
     # ENDPOINT: GET /bucketlists/<id>
-    # Allows users to get a bucketlist by specifying its id
     def test_users_can_get_bucketlist_using_id(self):
+        """Tests users can retrieve a bucketlist by specifying its id """
         res = self.client().post('/auth/login', data=self.user)
         res_data = json.loads(res.data)
         jwt_token = res_data.get('token')
@@ -50,9 +37,23 @@ class BucketListTestCase(BaseTestCase):
         rv = self.client().get('bucketlists/1', headers=headers)
         self.assertEqual(rv.status_code, 200)
 
+    # ENDPOINT: POST '/bucketlists'
+    def test_user_can_create_bucketlist(self):
+        """Tests users to create a new bucketlist """
+        res = self.client().post('/auth/login', data=self.user)
+        res_data = json.loads(res.data)
+        jwt_token = res_data.get('token')
+        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
+        rv = self.client().post(
+            '/bucketlists',
+            data={'name': 'Eat pray and love'}, headers=headers)
+        self.assertEqual(rv.status_code, 201)
+        # logout the user after testing
+        self.client().get('/auth/logout', headers=headers)
+
     # ENDPOINT: PUT /bucketlist/<id>
-    # Tests that user can edit an existing bucketlist
     def test_users_can_edit_bucketlist(self):
+        """Tests users can edit an existing bucketlist by id"""
         res = self.client().post('/auth/login', data=self.user)
         res_data = json.loads(res.data)
         jwt_token = res_data.get('token')
@@ -69,6 +70,16 @@ class BucketListTestCase(BaseTestCase):
             headers=headers)
         self.assertEqual(rv.status_code, 200)
 
-
-
-
+    # ENDPOINT: DELETE: '/bucketlist/<id>'
+    def test_users_can_delete_bucketlist(self):
+        """Tests users can delete their bucketlist by id """
+        res = self.client().post('/auth/login', data=self.user)
+        res_data = json.loads(res.data)
+        jwt_token = res_data.get('token')
+        headers = {'Authorization': 'Bearer {0}'.format(jwt_token)}
+        rv = self.client().post(
+            '/bucketlists',
+            data={'name': 'Eat pray and love'}, headers=headers)
+        self.assertEqual(rv.status_code, 201)
+        rv = self.client().delete('/bucketlists/1', headers=headers)
+        self.assertEqual(rv.status_code, 200)
