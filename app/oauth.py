9437d1ebefa702cdf7ff69520beed54e4750ca6a
jee <Andela_9@jee.local>
2016-01-12 11:11:03 +0300

    (feature) Implement Facebook Oauth configs

--- app/oauth.py
+++ app/oauth.py
@@ -34,7 +34,47 @@ class OAuthSignIn(object):
 
 
 class FacebookSignIn(OAuthSignIn):
-    pass
+    """Signs in using Facebook Oauth service"""
+    def __init__(self):
+        super(FacebookSignIn, self).__init__('facebook')
+        # Initialize service object with client_id and client_secret
+        self.service = OAuth2Service(
+            name='facebook',
+            client_id=self.consumer_id,
+            client_secret=self.consumer_secret,
+            authorize_url='https://graph.facebook.com/oauth/authorize',
+            access_token_url='https://graph.facebook.com/oauth/access_token',
+            base_url='https://graph.facebook.com/'
+        )
+
+    def authorize(self):
+        """Issues a redirect to a URL provided by the Rauth's service object
+        Invokes the route after authentication completes
+        """
+        return redirect(self.service.get_authorize_url(
+            scope='email',
+            response_type='code',
+            redirect_url=self.get_callback_url())
+        )
+
+    def callback(self):
+        if 'code' not in request.args:
+            return None, None, None
+        oauth_session = self.service.get_auth_session(
+            data={
+                'code': request.args['code'],
+                'grant_type': 'authorization_code',
+                'redirect_uri': self.get_callback_url()
+            }
+        )
+        me = oauth_session.get('me?fields=id, email').json()
+        # extract email's user because Fb doesn't
+        # provide usernames by default.
+        return (
+            'facebook$' + me['id'],
+            me.get('email').split('@')[0],
+            me.get('email')
+        )
 
 
 class TwitterSignIn(OAuthSignIn):
